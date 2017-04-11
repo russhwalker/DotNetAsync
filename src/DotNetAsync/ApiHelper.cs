19d9462d8ac12b0b66afde56908af9cd16d3f33e
russ <russ.h.walker@gmail.com>
2017-04-10 20:51:26 -0400

    Initial Commit

---
+++ src/DotNetAsync/ApiHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAsync
+{
+    public class ApiHelper
+    {
+
+        private readonly string baseUri;
+
+        public ApiHelper(string baseUri)
+        {
+            this.baseUri = baseUri;
+        }
+
+
+        public async Task<HttpResponseMessage> CallApiAsync(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return await client.GetAsync(uri);
+            }
+        }
+
+        public async Task<string> CallApiStringAsync(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return await client.GetStringAsync(uri);
+            }
+        }
+
+    }
+}
